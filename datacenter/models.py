beaea7bf9d82b53f1a2decadadc9dd55483b4b63
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-02-27 12:37:09 +0300

    Update models.py

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import localtime
 
 
 class Passcard(models.Model):
@@ -28,3 +29,30 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+
+    def get_duration(self):
+        """Возвращает результат вычисления продолжительности пребывания"""
+        entered = localtime(self.entered_at)
+        duration = localtime() - entered
+        if self.leaved_at:
+            duration = localtime(self.leaved_at) - entered
+        return duration
+
+    def format_duration(self):
+        """Форматирует duration в строковое выражение"""
+        duration = self.get_duration()
+        days, seconds = duration.days, duration.total_seconds()
+        hours = int(days * 24 + seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        seconds = int((seconds % 60))
+        format_duration = f"{hours} час. {minutes} мин."
+        return format_duration
+
+    def is_visit_long(self, minutes=10):
+        """
+        Возвращает результат сранвения продолжительности пребывания
+        с ограничением minutes
+        """
+        duration = self.get_duration()
+        duration_minutes = int(duration.total_seconds() // 60)
+        return duration_minutes > minutes

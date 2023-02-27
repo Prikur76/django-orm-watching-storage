2254587ae0dd3eeb4f170c2d10900bb280ed672c
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-02-27 12:39:43 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,16 +4,24 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    """Возвращает список незакрытых посещений"""
+    visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for visit in visits:
+        who_entered = Visit.objects.get(id=visit.id).passcard.owner_name
+        entered_at = visit.entered_at
+        duration = visit.format_duration()
+        is_strange = visit.is_visit_long()
+        non_closed_visits.append(
+            {
+                'who_entered': who_entered,
+                'entered_at': entered_at,
+                'duration': duration,
+                'is_strange': is_strange
+            }
+        )
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
